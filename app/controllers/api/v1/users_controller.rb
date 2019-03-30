c2e00c9b0d95e40dd2f93f29745897ce3614dfbc
Oluwatomi Duyile <thecodingpoet@gmail.com>
2019-03-30 12:29:05 +0100

    Add functionality to login

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -4,9 +4,21 @@ module Api
       def create
         user = User.new(user_params)
         if user.save
-          render json: {status: 'User created successfully'}, status: :created
+          auth_token = JsonWebToken.encode({user_id: user.id})
+          render json: {message: 'User created successfully', auth_token: auth_token}, status: :created
         else
           render json: { errors: user.errors.full_messages }, status: :bad_request
+        end
+      end
+
+      def login
+        user = User.find_by(email: params[:user][:email].to_s.downcase)
+        if user && user.authenticate(params[:user][:password])
+          auth_token = JsonWebToken.encode({user_id: user.id})
+          binding.pry
+          render json: {message: 'User logged in successfully', auth_token: auth_token}, status: :ok
+        else
+          render json: {error: 'Invalid username or password'}, status: :unauthorized
         end
       end
 
