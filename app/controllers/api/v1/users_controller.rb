c5b9e1cf99cdd9a2980882487e7622942570ef20
Oluwatomi Duyile <thecodingpoet@gmail.com>
2019-04-05 00:46:09 +0100

    Fix signup route bug

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -4,7 +4,7 @@ module Api
       skip_before_action :authenticate_request!, only: [:create, :login] 
       before_action :check_authorized, except: [:create, :login]
 
-      def create
+      def signup
         user = User.new(user_params)
         if user.save
           token = JsonWebToken.encode(user_id: user.id)
