ad91df392f38b388856d2bb740fa6bbdb7bb41b7
Oluwatomi Duyile <thecodingpoet@gmail.com>
2019-04-08 14:13:44 +0100

    Skip authenticate for root path

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,5 @@
 class HomeController < ApplicationController
+  skip_before_action :authenticate_request!, only: [:index] 
   def index
     render json: { message: "I-LU Backend API"}, status: :ok
   end
