97edcc60da55b46e5946727ae6f1bd8d432bd529
Oluwatomi Duyile <thecodingpoet@gmail.com>
2019-04-11 21:13:19 +0100

    Add education to user serializer

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,6 +1,6 @@
 class UserSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :email, :username, :avatar, :birth_year, :gender
+  attributes :email, :username, :avatar, :birth_year, :gender, :education
   has_many :strengths
   has_many :concerns
 end
