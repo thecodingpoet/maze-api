d9d2b3565d7dfcdd8a36c5eb3cd1121da5472bed
Oluwatomi Duyile <thecodingpoet@gmail.com>
2019-04-17 13:16:35 +0100

    Remove writing title uniqueness validation

--- app/models/writing.rb
+++ app/models/writing.rb
@@ -2,6 +2,6 @@ class Writing < ApplicationRecord
   belongs_to :user
   has_many :comments
 
-  validates :title, presence: true, :uniqueness => {:scope => [:user, :title]}
+  validates :title, presence: true
   validates :entry, presence: true
 end
