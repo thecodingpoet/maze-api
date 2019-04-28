226504350f89d33b91fca2a8f28c199d276bb4e0
Oluwatomi Duyile <thecodingpoet@gmail.com>
2019-04-28 19:55:16 +0100

    Update timeline

--- app/serializers/comment_serializer.rb
+++ app/serializers/comment_serializer.rb
@@ -1,6 +1,15 @@
 class CommentSerializer
   include FastJsonapi::ObjectSerializer
   attributes :content, :created_at, :approved
-  belongs_to :user
+
+  attributes :user do |comment|
+    {
+      id: comment.user.id,
+      username: comment.user.username,	
+      avatar: comment.user.avatar,	
+      created_at: comment.user.created_at
+    }
+  end
+
   belongs_to :writing
 end
