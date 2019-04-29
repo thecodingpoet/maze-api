52c4d6d033cc444f78c64d46d1bd6e253be3cc15
Oluwatomi Duyile <thecodingpoet@gmail.com>
2019-04-29 11:36:33 +0100

    Add date when user was created

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,6 +1,6 @@
 class UserSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :email, :username, :avatar, :birth_year, :gender, :education
+  attributes :email, :username, :avatar, :birth_year, :gender, :education, :created_at
   has_many :strengths
   has_many :concerns
 end
