23492be6f1b8e41445817cb15c7d4ccb6e543cee
Oluwatomi Duyile <thecodingpoet@gmail.com>
2019-06-01 17:37:49 +0100

    Remove thread accesses table

--- db/schema.rb
+++ db/schema.rb
@@ -62,16 +62,6 @@ ActiveRecord::Schema.define(version: 20190601123053) do
     t.index ["user_id"], name: "index_strengths_on_user_id"
   end
 
-  create_table "thread_accesses", force: :cascade do |t|
-    t.bigint "user_id"
-    t.bigint "writing_id"
-    t.boolean "access", default: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_thread_accesses_on_user_id"
-    t.index ["writing_id"], name: "index_thread_accesses_on_writing_id"
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "email", null: false
     t.string "password_digest", null: false
@@ -107,7 +97,5 @@ ActiveRecord::Schema.define(version: 20190601123053) do
   add_foreign_key "concerns", "users"
   add_foreign_key "feedbacks", "users"
   add_foreign_key "strengths", "users"
-  add_foreign_key "thread_accesses", "users"
-  add_foreign_key "thread_accesses", "writings"
   add_foreign_key "writings", "users"
 end
