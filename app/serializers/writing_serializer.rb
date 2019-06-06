17bbd1f621fccb6ad0aa41eb570d4b99317b94eb
Oluwatomi Duyile <thecodingpoet@gmail.com>
2019-06-06 13:22:02 +0100

    Include comments count when showing writings

--- app/serializers/writing_serializer.rb
+++ app/serializers/writing_serializer.rb
@@ -1,6 +1,6 @@
 class WritingSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :title, :entry, :status, :created_at
+  attributes :title, :entry, :status, :created_at, :comments_count
 
   belongs_to :user
   has_many :comments
