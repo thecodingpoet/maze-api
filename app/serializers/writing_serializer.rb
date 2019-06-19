2cafb398edfbaba721c0b630beaed415228e08ef
Oluwatomi Duyile <thecodingpoet@gmail.com>
2019-06-19 14:24:20 +0100

    Update Writing Serializer
    * Update comments count to only include comment that is not declined.
    * Add pending approval count

--- app/serializers/writing_serializer.rb
+++ app/serializers/writing_serializer.rb
@@ -1,6 +1,14 @@
 class WritingSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :title, :entry, :status, :created_at, :comments_count
+  attributes :title, :entry, :status, :created_at
+
+  attribute :comments_count do |writing|
+    writing.comments.not_declined.count
+  end
+
+  attribute :pending_approval_count do |writing|
+    writing.comments.pending_approval.count
+  end
 
   belongs_to :user
   has_many :comments
